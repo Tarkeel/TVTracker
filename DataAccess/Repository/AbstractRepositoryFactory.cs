3476feb1e457b199a28206e3cbe88df5c14b3d5a
Tarkeel <tarkeel@gmail.com>
2015-09-13 13:30:10 +0200

    Repository contract

---
+++ DataAccess/Repository/AbstractRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public abstract class AbstractRepositoryFactory
+    {
+        public abstract IShowRepository ShowRepository { get; }
+        public abstract ISeasonRepository SeasonRepository { get; }
+        public abstract IEpisodeRepository EpisodeRepository { get; }
+        public abstract IExternalSourceRepository ExternalSourceRepository { get; }
+        public abstract IExternalShowRepository ExternalShowRepository { get; }
+        public abstract IConfigurationRepository ConfigurationRepository { get; }
+        public abstract void Persist();
+    }
+}
---
+++ DataAccess/Repository/IConfigurationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Repository
+{
+    public interface IConfigurationRepository
+    {
+        string GetValue(string Setting);
+        void SetValue(string Setting, string Value, bool persist = true);
+        void ClearSetting(string Setting, bool persist = true);
+    }
+}
